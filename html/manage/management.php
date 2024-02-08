a82d3192d65f570bb5c0cfb55f43ae9a872ba319
Ascien <quinchon.raphael.061@gmail.com>
2024-02-08 01:40:50 +0100

    Adding a management page

---
+++ html/manage/management.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <!--Head informations-->
+    
+        <title>Garage V.Parrot | Connexion</title>
+        <meta charset="utf-8">
+        <meta name="author" content="Raphaël Quinchon">
+        <meta name="description" content="Garage V.Parrot | Mecanique, réparation, carrosserie, ventes d'occasion">
+        <meta name="robots" content="noindex">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+        <!--Import CSS-->
+        <link href="../../css/management.css" rel="stylesheet">
+    </head>
+    <body>
+        <header>
+            <a href="../../index.php"><img id="logo" src="../../elements/images/logo.svg" alt="logo-garage"></a>
+            <h1>Espace de gestion</h1>
+        </header>
+        <main>
+            <nav>
+                <button>Pages</button> <!--Admin seulement-->
+                <button>Avis</button> <!--Modération et ajout des avis client-->
+                <button>Contact</button>
+                <button>Ventes</button>
+                <button>Utilisateurs</button> <!--Admin seulement-->
+                <button>Horraires</button> <!--Admin seulement-->
+            </nav>
+        </main>
+    </body>
+</html>
